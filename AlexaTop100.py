950518acfcddd5112f290e1290b2bf9fbb8cb17b
W R <willem1993@hotmail.com>
2017-01-10 21:54:36 +0100

    Create AlexaTop100.py

---
+++ AlexaTop100.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
+amountOfPages = 4
+top100Sites = list()
+
+for i in range(amountOfPages):
+    r = requests.get(f'http://www.alexa.com/topsites/countries;{i}/NL')
+    soup = BeautifulSoup(r.text, 'html.parser')
+    sites = soup.find_all('p', {'class':'desc-paragraph'})
+    for site in sites:
+        top100Sites.append(site.text.strip())
+
+print(top100Sites)
